ec680817b52a64807218fc98634c0d6f6cbaaef6
Júlio Rodrigues <116827694+MegaPlys@users.noreply.github.com>
2024-05-06 21:21:38 -0300

    C27 - Resgatando projetos do banco de dados
    adicionado o fetch do banco de dados no "/projeto" para gerenciar projetos do banco de dados

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,12 +1,18 @@
 import { useLocation } from "react-router-dom";
 
+import { useState, useEffect } from "react";
+
 import Message from "../layout/Message";
 import Container from "../layout/Container";
 import LinkButton from "../layout/LinkButton";
+import ProjectCard from "../project/ProjectCard";
 
 import styles from "./Projects.module.css"
 
+
 function Projects() {
+    // const pra salvar os projetos
+    const [projects, setProjects] = useState([])
 
     const location = useLocation()
 
@@ -16,6 +22,20 @@ function Projects() {
         message = location.state.message
     }
 
+    useEffect(() => {
+
+        fetch('http://localhost:5000/projects', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        }).then((resp) => resp.json())
+        .then((data) => {
+            setProjects(data)
+        })
+        .catch((err) => console.log(err))
+    }, [])
+
     return (
         <div className={styles.project_container}>
             <div className={styles.tittle_container}>
@@ -24,7 +44,14 @@ function Projects() {
             </div>
             {message && <Message type="sucess" msg={message} />}
             <Container customClass="start">
-                <p>Projetos...</p>
+                {projects.length > 0 &&
+                    projects.map((project) => <ProjectCard 
+                    id={project.id}
+                    name={project.name} 
+                    budget={project.budget}
+                    category={project.category.name}
+                    key={project.id}
+                    />)}
             </Container>
         </div>
     )
