95890845f9611f0cbe325f3ff3b5348e0b81eef0
Júlio Rodrigues <116827694+MegaPlys@users.noreply.github.com>
2024-05-10 16:36:28 -0300

    C28 - Criando componente Loader
    adicionado ao projeto o componente Loader, que é um svg que serve pra mostrar que está carregando a página.

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -4,6 +4,7 @@ import { useState, useEffect } from "react";
 
 import Message from "../layout/Message";
 import Container from "../layout/Container";
+import Loading from "../layout/Loading";
 import LinkButton from "../layout/LinkButton";
 import ProjectCard from "../project/ProjectCard";
 
@@ -13,6 +14,7 @@ import styles from "./Projects.module.css"
 function Projects() {
     // const pra salvar os projetos
     const [projects, setProjects] = useState([])
+    const [removeLoading, setRemoveLoading] = useState(false)
 
     const location = useLocation()
 
@@ -22,27 +24,36 @@ function Projects() {
         message = location.state.message
     }
 
-    useEffect(() => {
-
-        fetch('http://localhost:5000/projects', {
+    useEffect(() => { 
+        // setTimeout só pra mostrar loader na tela
+        setTimeout(() => {
+//useEffect para pegar do db.json os projetos salvos 
+            fetch('http://localhost:5000/projects', {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
             },
         }).then((resp) => resp.json())
         .then((data) => {
+            console.log(data)
             setProjects(data)
+            setRemoveLoading(true)
         })
         .catch((err) => console.log(err))
+
+        }, 300)
     }, [])
 
     return (
         <div className={styles.project_container}>
             <div className={styles.tittle_container}>
                 <h1>Meus Projetos</h1>
-                <LinkButton to="/newproject" text="Criar Projeto" />
+                { /* link botão para criar novo projeto */ }
+                <LinkButton to="/newproject" text="Criar Projeto" />  
             </div>
             {message && <Message type="sucess" msg={message} />}
+
+            { /* Área de todos os projetos listados */ }
             <Container customClass="start">
                 {projects.length > 0 &&
                     projects.map((project) => <ProjectCard 
@@ -51,7 +62,13 @@ function Projects() {
                     budget={project.budget}
                     category={project.category.name}
                     key={project.id}
-                    />)}
+                />)}
+                {/* svg loading aqui */}
+                {!removeLoading && <Loading />}
+                {removeLoading && projects.lenght === 0 && (
+                    <p>Não há Projetos cadastrados  </p>
+                )
+                }
             </Container>
         </div>
     )
