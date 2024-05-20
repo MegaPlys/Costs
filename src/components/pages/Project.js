7be7f405b673904626892804cc9c3a64c44fb997
Júlio Rodrigues <116827694+MegaPlys@users.noreply.github.com>
2024-05-20 10:59:29 -0300

    C32 - Atualização do projeto
    adicionado a função de editar projeto por meio de formulário

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -1,6 +1,8 @@
 import styles from './Project.module.css'
 import Loading from '../layout/Loading'
 import Container from '../layout/Container'
+import ProjectForm from '../project/ProjectForm'
+import Message from '../layout/Message'
 
 import { useParams } from 'react-router-dom'
 import { useState, useEffect } from 'react'
@@ -15,6 +17,10 @@ function Project() {
 
     // Define o estado para controlar a exibição do formulário de edição
     const [showProjectForm, setShowProjectForm] = useState(false)
+
+     // Define o estado para mensagens de feedback
+    const [message, setMessage] = useState('')
+    const [type, setType] = useState('')
 
     useEffect(() => {
 
@@ -34,6 +40,32 @@ function Project() {
         }, 300)
         
     }, [id])
+
+    function editPost(project) {
+
+        //budget validation
+        if(project.budget < project.cost) {
+            setMessage('O orçamento não pode ser menor que o custo do projeto!')
+            setType('error')
+            return false
+        }
+
+        fetch(`http://localhost:5000/projects/${project.id}`,{
+            method: 'PATCH',
+            headers: {
+                'Content-Type':'application/json',
+            },
+            body: JSON.stringify(project),
+        })
+        .then((resp) => resp.json())
+        .then((data) => {
+            setProject(data)
+            setShowProjectForm(false)
+            setMessage('Projeto atualizado!')
+            setType('success')
+        })
+        .catch(err => console.log(err))
+    }
     
     // Função para alternar a exibição do formulário de edição
     function toggleProjectForm() { 
@@ -47,6 +79,7 @@ function Project() {
         {project.name ? (
         <div className={styles.project_details}>
             <Container customClass="column"> 
+                {message && <Message type={type} msg={message}/>}
                 <div className={styles.details_container}>
                     <h1>Projeto: {project.name}</h1>
                     <button className={styles.btn} onClick={toggleProjectForm}>
@@ -66,7 +99,11 @@ function Project() {
                         </div>
                     ) : ( // editar projeto
                         <div className={styles.project_info}>
-                            <p>formulario</p>
+                            <ProjectForm 
+                            handleSubmit={editPost}
+                            btnText="Concluir edição"
+                            projectData={project}
+                            />
                         </div>
                     )}
                 </div>
